b95019934a1531bc77352d6cf1a9008d09ba63b1
Ramon Ferreira <ramonfsk@gmail.com>
2020-06-04 21:21:43 -0300

    explicacao e criacao da estrutura basica do projeto

---
+++ server/src/server.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+/**
+ *  GET:    Buscar uma ou mais informações do back-end;
+ *  POST:   Criar uma nova informação no back-end;
+ *  PUT:    Atualizar uma informação existente no back-end;
+ *  DELETE: Remover uma informação do back-end;
+ * 
+ *  Exemplos:
+ *  POST -> http://localhost:3333/users = Cria um usuário;
+ *  GET  -> http://localhost:3333/users = Lista usuários;
+ *  GET  -> http://localhost:3333/users/5 = Busca dados do usuário com id 5
+ * 
+ * Request Param: Parâmetros que vem na própria rota que identificam um recurso;
+ * Query Param: Parâmetros que vem na própria rota, geralmente opcionais para filtros, paginação etc...;
+ * Request Body: Parâmetros para criação/atualização de informações; 
+ */
+
+const users = [
+    'Amanda',
+    '&',
+    'Ramon'
+];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
+});
+
+app.listen(3333);
