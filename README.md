e35fe5a986b3ad418237094891b8799eb1d12451
Ramon Ferreira <ramonfsk@gmail.com>
2020-06-06 15:51:27 -0300

    reforcando os conceitos do react

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# nlw
+# Next Level Week
 Repositório com os código das aulas da Next Level Week, ministrado pelo Sr. @diego3g CTO na @rocketseat.
 
 *💜 -> /server/src/serves.ts, lines 31 to 33*
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
-      </header>
+    <div>
+       <Header title={ `Contador: ${counter}` } />
+       <button onClick={ handleButtonClick }>Aumentar</button>
     </div>
   );
 }
 
 export default App;
+
+// JSX: Sintaxe de XML dentro do JavaScript
